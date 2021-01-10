289529fbed66227681bcd3f10fb97adbbda9afa5
Deniz Sarioz <sarioz@acm.org>
2021-01-10 16:46:46 +0300

    set the same suffix size for each label

--- Capstone-Project/08_6.1.baselining_for_capstone/automl_massage.bash
+++ Capstone-Project/08_6.1.baselining_for_capstone/automl_massage.bash
@@ -5,15 +5,15 @@ grep '\tnegative' ../train_simple.tsv | cut -f 1 > 0/negatives.txt
 grep '\tneutral' ../train_simple.tsv | cut -f 1 > 1/neutrals.txt
 grep '\tpositive' ../train_simple.tsv | cut -f 1 > 2/positives.txt
 cd 0
-split -l 10 negatives.txt
+# we use the high suffix length of 3 allowing up to 17,576 files
+split -a 3 -l 10 negatives.txt
 for f in x*; do mv "$f" "$f.txt"; done
 rm negatives.txt
 cd ../1
-split -l 10 neutrals.txt
+split -a 3 -l 10 neutrals.txt
 for f in x*; do mv "$f" "$f.txt"; done
 rm neutrals.txt
 cd ../2/
-# set a higher suffix length here, otherwise caps at 676 output files
 split -a 3 -l 10 positives.txt
 for f in x*; do mv "$f" "$f.txt"; done
 rm positives.txt
