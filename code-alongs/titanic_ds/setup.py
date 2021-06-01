509fcd876f7263b88c2ce83613e920bf1184b967
Deniz Sarioz <sarioz@acm.org>
2021-06-01 16:13:36 +0300

    add titanic_ds project initial state

---
+++ code-alongs/titanic_ds/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+
+def readme():
+    """Import the README.md Markdown file and try to convert it to RST format."""
+    try:
+        import pypandoc
+        return pypandoc.convert('README.md', 'rst')
+    except(IOError, ImportError):
+        with open('README.md') as readme_file:
+            return readme_file.read()
+
+
+setup(
+    name='titanic',
+    version='0.1',
+    description='Analysis of the Titanic dataset',
+    long_description=readme(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    url='https://github.com/sarioz/Springboard/code-alongs/titanic_ds',
+    author='Deniz Sarioz',
+    author_email='sarioz@acm.org',
+    license='MIT',
+    packages=['titanic'],
+)
