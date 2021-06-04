ea1dbc4538ca18ecfc30949cc80062f030a00a5d
Deniz Sarioz <sarioz@acm.org>
2021-06-04 14:36:12 +0300

    Add minimal noiser test based on random seed

---
+++ Capstone-Project/SpellChecker/noiser_test.py
@@ -0,0 +1,28 @@
+import unittest
+
+from noiser import DisjointNoiser
+
+import random
+
+class NoiserTestCase(unittest.TestCase):
+
+    def test_noisings(self):
+        random.seed(42)
+        noiser = DisjointNoiser()
+
+        clean_tweets = [
+            'echa un vistazo a nuestros planes móviles y descubre las opciones .',
+            'pues yo solo digo que hoy os enamoráis de mí'
+        ]
+
+        noisy_tweets_as_lists = [noiser.add_noise(list(t)) for t in clean_tweets]
+        noisy_tweets_readable = [''.join(t) for t in noisy_tweets_as_lists]
+
+        # The resulting noisy tweets depends on the random seed set above.
+        self.assertEqual(noisy_tweets_readable, [
+            'ch un vistazo a nstrs planes mviles y descubre las opcins .',
+            'pues yo solo digo que hoy os enamorais de mi'
+        ])
+
+if __name__ == '__main__':
+    unittest.main()
