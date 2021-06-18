92cc801f341b9b57e3d40fb9c7de4959959a190e
Deniz Sarioz <sarioz@acm.org>
2021-06-18 17:43:26 +0300

    Add training/inference generation pair for 2 Dense layers

--- Capstone-Project/SpellChecker/nn_model_creator.py
+++ Capstone-Project/SpellChecker/nn_model_creator.py
@@ -180,8 +180,67 @@ class NNModelCreator:
 
         return encoder_model, decoder_model
 
+    def create_bilstm_2dense_training_model(self) -> Model:
+        # Define an input sequence and process it.
+        encoder_inputs = Input(shape=(None, LEN_NN_VOCAB))
+        encoder = Bidirectional(LSTM(self.latent_dim, return_state=True), name="encoder_bilstm")
+        _, forward_h, forward_c, backward_h, backward_c = encoder(encoder_inputs)
+        state_h = Concatenate()([forward_h, backward_h])
+        state_c = Concatenate()([forward_c, backward_c])
+        encoder_states = [state_h, state_c]
+
+        # Set up the decoder, using `encoder_states` as initial state.
+        decoder_inputs = Input(shape=(None, LEN_NN_VOCAB))
+
+        # We set up our decoder to return full output sequences,
+        # and to return internal states as well. We don't use the
+        # return states in the training model, but we will use them in inference.
+        decoder_lstm = LSTM(self.latent_dim * 2, return_sequences=True, return_state=True, name="decoder_lstm")
+        decoder_outputs, _, _ = decoder_lstm(decoder_inputs, initial_state=encoder_states)
+        decoder_dense_1 = Dense(LEN_NN_VOCAB, activation="tanh", name="decoder_dense_1")
+        decoder_outputs = decoder_dense_1(decoder_outputs)
+        decoder_dense_2 = Dense(LEN_NN_VOCAB, activation="softmax", name="decoder_dense_2")
+        decoder_outputs = decoder_dense_2(decoder_outputs)
+
+        # Define the model that will turn
+        # `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
+        model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+        model.compile(
+            optimizer="rmsprop", loss="categorical_crossentropy", metrics=["accuracy"]
+        )
+
+        return model
+
+    def create_bilstm_2dense_inference_models(self, training_model: Model) -> (Model, Model):
+        encoder_inputs = training_model.input[0]
+        _, forward_h, forward_c, backward_h, backward_c = training_model.get_layer(name="encoder_bilstm").output
+        state_h_enc = Concatenate()([forward_h, backward_h])
+        state_c_enc = Concatenate()([forward_c, backward_c])
+        encoder_states = [state_h_enc, state_c_enc]
+        encoder_model = Model(encoder_inputs, encoder_states)
+
+        decoder_inputs = training_model.input[1]
+        decoder_state_input_h = Input(shape=(self.latent_dim * 2,), name="decoder_state_input_h")
+        decoder_state_input_c = Input(shape=(self.latent_dim * 2,), name="decoder_state_input_c")
+        decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+        decoder_lstm = training_model.get_layer("decoder_lstm")
+        decoder_outputs, state_h_dec, state_c_dec = decoder_lstm(
+            decoder_inputs, initial_state=decoder_states_inputs
+        )
+        decoder_states = [state_h_dec, state_c_dec]
+        decoder_dense_1 = training_model.get_layer(name="decoder_dense_1")
+        decoder_outputs = decoder_dense_1(decoder_outputs)
+        decoder_dense_2 = training_model.get_layer(name="decoder_dense_2")
+        decoder_outputs = decoder_dense_2(decoder_outputs)
+
+        decoder_model = Model(
+            [decoder_inputs] + decoder_states_inputs, [decoder_outputs] + decoder_states
+        )
+
+        return encoder_model, decoder_model
+
     def create_training_model(self) -> Model:
-        m = self.create_bilstm_training_model()
+        m = self.create_bilstm_2dense_training_model()
         print('training model:')
         m.summary()
 
@@ -190,7 +249,7 @@ class NNModelCreator:
     def derive_inference_models(self, training_model: Model) -> (Model, Model):
         print('loaded training_model:')
         training_model.summary()
-        encoder_model, decoder_model = self.derive_bilstm_inference_models(training_model)
+        encoder_model, decoder_model = self.create_bilstm_2dense_inference_models(training_model)
         print('derived encoder_model:')
         encoder_model.summary()
         print('derived decoder_model:')
