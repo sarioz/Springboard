4e5befd395ab060c9367d8dd8b4ca1edf6f26eb1
Deniz Sarioz <sarioz@acm.org>
2021-08-02 19:09:25 +0300

    Calculate accuracy scores

--- Capstone-Project/POSTagger-RNN/main_inference.py
+++ Capstone-Project/POSTagger-RNN/main_inference.py
@@ -27,7 +27,7 @@ def main_inference():
     vu = VocabUtil(sorted_training_tokens)
     nn_input_preparer = NNInputPreparer(vu)
 
-    k = 10
+    k = 1000
     for input_filename in [TRAINING_INPUT_FILENAME, DEV_INPUT_FILENAME]:
         print(f'processing the first {k} tweets from {input_filename}')
         loader = LabeledDataLoader(input_filename)
@@ -42,9 +42,11 @@ def main_inference():
         rectangular_inputs = nn_input_preparer.rectangularize_inputs(irregular_inputs)[:k]
         rectangular_targets = nn_input_preparer.rectangularize_targets(irregular_targets)[:k]
 
-        print("token\tpredicted\ttarget\n")
-
+        # print("token\tpredicted\ttarget\n")
+        predictions = correct_predictions = 0
+        tweet_accuracy_sum = 0.0
         for rectangular_input, rectangular_target in zip(rectangular_inputs, rectangular_targets):
+            tweet_level_predictions = tweet_level_correct_predictions = 0
             tokens_human = [vu.nn_input_tokens[i] for i in rectangular_input if i != 0]
             predicted = loaded_training_model.predict(rectangular_input)
             # could also consider sampling from the probability distribution
@@ -53,8 +55,18 @@ def main_inference():
             target_indices = [rectangular_target[i] for i in range(len(tokens_human))]
             target_human = [vu.nn_pos_tuple[i] for i in target_indices]
             for tok, pred, tar in zip(tokens_human, predicted_human, target_human):
-                print(f"{tok}\t{pred}\t{tar}")
-            print()
+                predictions += 1
+                tweet_level_predictions += 1
+                # print(f"{tok}\t{pred}\t{tar}")
+                if pred == tar:
+                    correct_predictions += 1
+                    tweet_level_correct_predictions += 1
+            tweet_accuracy = tweet_level_correct_predictions / tweet_level_predictions
+            tweet_accuracy_sum += tweet_accuracy
+            # print()
+
+        print(f'Token-level accuracy for {input_filename}:', correct_predictions / predictions)
+        print(f'Tweet-level accuracy for {input_filename}:', tweet_accuracy_sum / len(rectangular_inputs))
 
 
 if __name__ == '__main__':
