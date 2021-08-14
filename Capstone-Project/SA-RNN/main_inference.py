cce84e6d7cf1211fe000c51cf290816ecae3af65
Deniz Sarioz <sarioz@acm.org>
2021-08-14 15:49:23 +0300

    Measure expected sampling accuracy properly

--- Capstone-Project/SA-RNN/main_inference.py
+++ Capstone-Project/SA-RNN/main_inference.py
@@ -1,14 +1,13 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import load_model
-from numpy.random import default_rng
+from tqdm import tqdm
 
 from labeled_data_loader import LabeledDataLoader
-from nn_input_preparer import NNInputPreparer
 from vocab_util import VocabUtil
 
 EXPERIMENT_NAME = '03_bi_LSTM_256_256'
-TRAINING_MODEL_FILENAME = f'models/{EXPERIMENT_NAME}/ep_20_valacc_0.85574.h5'
+TRAINING_MODEL_FILENAME = f'models/{EXPERIMENT_NAME}/ep_20_valacc_0.88770.h5'
 
 TRAINING_INPUT_FILENAME = '../data/sa/train.conll'
 DEV_INPUT_FILENAME = '../data/sa/dev.conll'
@@ -16,8 +15,6 @@ DEV_INPUT_FILENAME = '../data/sa/dev.conll'
 
 def main_inference():
     print(f'Using TensorFlow version {tf.__version__}')
-
-    rng = default_rng()
 
     print(f'Loading model {TRAINING_MODEL_FILENAME}')
     loaded_training_model = load_model(TRAINING_MODEL_FILENAME)
@@ -29,14 +26,11 @@ def main_inference():
     sorted_training_tokens = sorted(unique_training_tokens)
     # we should instantiate a vocab util only based on the training tokens, not dev/test tokens
     vu = VocabUtil(sorted_training_tokens)
-    nn_input_preparer = NNInputPreparer(vu)
 
     for input_filename in [TRAINING_INPUT_FILENAME, DEV_INPUT_FILENAME]:
-    # for input_filename in [TRAINING_INPUT_FILENAME]:
-        k = 500
-        print(f'processing the first {k} tweets from {input_filename}')
         loader = LabeledDataLoader(input_filename)
         labeled_tweets = loader.parse_tokens_and_labels(loader.load_lines())
+        print(f'processing all {len(labeled_tweets)} tweets from {input_filename}')
 
         irregular_inputs = [[vu.nn_input_token_to_int[token]
                              if token in vu.nn_input_token_to_int
@@ -44,31 +38,25 @@ def main_inference():
                              for token in labeled_tweet[0]]
                             for labeled_tweet in labeled_tweets]
 
-        # rectangular_inputs = nn_input_preparer.rectangularize_inputs(irregular_inputs[:k])
-        rectangular_targets = [labeled_tweet[1] for labeled_tweet in labeled_tweets[:k]]
-        # predicted_probabilities_sequence = loaded_training_model.predict(rectangular_inputs)
+        rectangular_targets = [labeled_tweet[1] for labeled_tweet in labeled_tweets]
 
-        matches_argmax = matches_sampling = 0
-        for i, target in enumerate(rectangular_targets):
-            # print('irregular_inputs[i]:', irregular_inputs[i])
-            # print('len(irregular_inputs[i]):', len(irregular_inputs[i]))
-            # print('target:', target)
-            predicted_probabilities_2 = loaded_training_model.predict([irregular_inputs[i]])[0]
-            # print('predicted_probabilities_2.shape:', predicted_probabilities_2.shape)
-            # print(predicted_probabilities_2)
+        expected_sampling_accuracy_sum = 0.0
+        num_correct_argmax_predictions = 0
+        for irregular_input, target_human in tqdm(zip(irregular_inputs, rectangular_targets)):
+            target_index = vu.nn_rsl_to_int[target_human]
+            predicted_probabilities = loaded_training_model.predict(irregular_input)[0]
+            # the predicted index if we take the class with the largest probability
+            if np.argmax(predicted_probabilities) == target_index:
+                num_correct_argmax_predictions += 1
+            # rhs is the probability of guessing target_index if we sample according to predicted probabilities
+            expected_sampling_accuracy_sum += predicted_probabilities[target_index]
 
-            predicted_label_argmax = np.argmax(predicted_probabilities_2)
-            predicted_human_argmax = vu.raw_sentiment_labels[predicted_label_argmax]
-            if predicted_human_argmax == target:
-                matches_argmax += 1
+        num_tweets_in_dataset = len(rectangular_targets)
 
-            predicted_label_sampling = rng.choice(3, 1, p=predicted_probabilities_2)[0]
-            predicted_human_sampling = vu.raw_sentiment_labels[predicted_label_sampling]
-            if predicted_human_sampling == target:
-                matches_sampling += 1
-
-        print('accuracy_argmax:', matches_argmax / k)
-        print('accuracy_sampling:', matches_sampling / k)
+        print(f'Argmax accuracy for {input_filename}:',
+              num_correct_argmax_predictions / num_tweets_in_dataset)
+        print(f'Expected sampling accuracy for {input_filename}:',
+              expected_sampling_accuracy_sum / num_tweets_in_dataset)
 
 
 if __name__ == '__main__':
