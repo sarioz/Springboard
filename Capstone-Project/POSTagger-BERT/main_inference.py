20838641defdd85c3378aeebfacb2beac2d1c04d
Deniz Sarioz <sarioz@acm.org>
2021-08-22 18:10:40 +0300

    Add evaluation with padding

--- Capstone-Project/POSTagger-BERT/main_inference.py
+++ Capstone-Project/POSTagger-BERT/main_inference.py
@@ -32,9 +32,9 @@ def main_inference():
     btc = BertTokenConverter(BERT_PRETRAINED_MODEL_DIR, tvu)
     nn_input_preparer = NNInputPreparer(tvu=tvu, max_seq_len=MAX_SEQ_LEN)
 
-    for input_filename in [TRAINING_INPUT_FILENAME, DEV_INPUT_FILENAME]:
+    for input_filename in [DEV_INPUT_FILENAME]:
         loader = LabeledDataLoader(input_filename)
-        tweets = loader.parse_raw_tokens_and_labels(loader.load_lines())
+        tweets = loader.parse_raw_tokens_and_labels(loader.load_lines())[:10]
         tweets = btc.convert_to_tokens(tweets)
         tweets = btc.convert_to_ids(tweets)
         tweets = nn_input_preparer.filter_out_long_sequences(tweets)
@@ -42,26 +42,38 @@ def main_inference():
         irregular_inputs = [[item[0] for item in tweet] for tweet in tweets]
         irregular_targets = [[item[1] for item in tweet] for tweet in tweets]
 
-        num_tokens_in_dataset = num_token_level_correct_argmax_predictions = 0
+        num_tokens_in_dataset = num_token_level_correct_argmax_predictions = \
+            num_token_level_correct_argmax_predictions_incl_pads = 0
         tweet_level_argmax_accuracy_sum = tweet_level_expected_sampling_accuracy_sum = \
-            token_level_expected_sampling_accuracy_sum = 0.0
+            token_level_expected_sampling_accuracy_sum = token_level_expected_sampling_accuracy_sum_incl_pads = 0.0
+
         for (irregular_input, irregular_target_indices) in tqdm(zip(irregular_inputs, irregular_targets)):
-            num_current_tweet_correct_argmax_predictions = 0
-            current_tweet_expected_sampling_accuracy_sum = 0.0
             rectangular_inputs = nn_input_preparer.rectangularize_inputs([irregular_input])
-            predicted_probability_sequence = trained_model.predict(rectangular_inputs)[0]
-            for predicted_probabilities, target_index in zip(predicted_probability_sequence, irregular_target_indices):
+            rectangular_targets = nn_input_preparer.rectangularize_targets([irregular_target_indices])
+            num_tokens_in_current_tweet = num_current_tweet_correct_argmax_predictions = 0
+            current_tweet_expected_sampling_accuracy_sum = 0.0
+            predicted_probabilities_sequence = trained_model.predict(rectangular_inputs)
+            for predicted_probabilities, target_index in zip(
+                    predicted_probabilities_sequence[0], rectangular_targets[0]):
                 # the predicted index if we take the class with the largest probability
-                if np.argmax(predicted_probabilities) == target_index:
-                    num_token_level_correct_argmax_predictions += 1
-                    num_current_tweet_correct_argmax_predictions += 1
+                argmax_index = np.argmax(predicted_probabilities)
                 # probability of guessing target_index if we sample according to predicted probabilities
                 prob_sampling_success_on_token = predicted_probabilities[target_index]
-                current_tweet_expected_sampling_accuracy_sum += prob_sampling_success_on_token
-                token_level_expected_sampling_accuracy_sum += prob_sampling_success_on_token
-            num_tokens_in_current_tweet = len(irregular_input)
-            num_tokens_in_dataset += num_tokens_in_current_tweet
+                if argmax_index == target_index:
+                    num_token_level_correct_argmax_predictions_incl_pads += 1
+                token_level_expected_sampling_accuracy_sum_incl_pads += prob_sampling_success_on_token
 
+                if target_index != 0:
+                    if argmax_index == target_index:
+                        num_token_level_correct_argmax_predictions += 1
+                        num_current_tweet_correct_argmax_predictions += 1
+                    current_tweet_expected_sampling_accuracy_sum += prob_sampling_success_on_token
+                    token_level_expected_sampling_accuracy_sum += prob_sampling_success_on_token
+                    num_tokens_in_current_tweet += 1
+                    num_tokens_in_dataset += 1
+
+            print('num_tokens_in_current_tweet:', num_tokens_in_current_tweet)
+            # every tweet has at least one non-padding token, so we don't worry about division by zero
             current_tweet_argmax_accuracy = num_current_tweet_correct_argmax_predictions / num_tokens_in_current_tweet
             current_tweet_expected_sampling_accuracy = \
                 current_tweet_expected_sampling_accuracy_sum / num_tokens_in_current_tweet
@@ -69,12 +81,19 @@ def main_inference():
             tweet_level_argmax_accuracy_sum += current_tweet_argmax_accuracy
             tweet_level_expected_sampling_accuracy_sum += current_tweet_expected_sampling_accuracy
 
+        num_tweets_in_dataset = len(tweets)
+        num_tokens_in_dataset_incl_pads = MAX_SEQ_LEN * num_tweets_in_dataset
+
+        print(f'Argmax accuracy for {input_filename} including padding:',
+              num_token_level_correct_argmax_predictions_incl_pads / num_tokens_in_dataset_incl_pads)
+        print(f'Expected sampling accuracy for {input_filename} including padding:',
+              token_level_expected_sampling_accuracy_sum_incl_pads / num_tokens_in_dataset_incl_pads)
+
         print(f'Token-level argmax accuracy for {input_filename}:',
               num_token_level_correct_argmax_predictions / num_tokens_in_dataset)
         print(f'Token-level expected sampling accuracy for {input_filename}:',
               token_level_expected_sampling_accuracy_sum / num_tokens_in_dataset)
 
-        num_tweets_in_dataset = len(irregular_inputs)
         print(f'Tweet-level argmax accuracy for {input_filename}:',
               tweet_level_argmax_accuracy_sum / num_tweets_in_dataset)
         print(f'Tweet-level expected sampling accuracy for {input_filename}:',
