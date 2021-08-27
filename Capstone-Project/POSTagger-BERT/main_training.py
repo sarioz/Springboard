623171a103066c42523be2224cd04d58dbe7be54
Deniz Sarioz <sarioz@acm.org>
2021-08-27 15:29:37 +0300

    Rename some variables

--- Capstone-Project/POSTagger-BERT/main_training.py
+++ Capstone-Project/POSTagger-BERT/main_training.py
@@ -11,19 +11,19 @@ from bert_token_converter import BertTokenConverter
 
 from nn_input_preparer import NNInputPreparer
 
-BERT_PRETRAINED_MODEL_DIR = "../multi_cased_L-12_H-768_A-12/"
+BERT_PRETRAINED_MODEL_DIR = '../multi_cased_L-12_H-768_A-12/'
 
 MAX_SEQ_LEN = 128
 
-EXPERIMENT_NAME = f'05_MLBERT_frozen_on_dev'
+EXPERIMENT_NAME = '06_MLBERT_AdamW_frozen_dev'
 MAX_EPOCHS = 50
 
 BASE_DIR = f'models/{EXPERIMENT_NAME}/'
 FINAL_TRAINED_MODEL_FILENAME = BASE_DIR + 'trained_model.h5'
 
-CONTINUE_TRAINING = False
-INITIAL_EPOCH = 2 if CONTINUE_TRAINING else 0
-TRAINING_MODEL_FILENAME_TO_CONTINUE = BASE_DIR + 'ep_2_valacc_0.98046.h5'
+CONTINUE_TRAINING = True
+INITIAL_EPOCH = 19 if CONTINUE_TRAINING else 0
+TRAINING_MODEL_FILENAME_TO_CONTINUE = BASE_DIR + 'ep_19_valacc_0.98182.h5'
 
 TRAINING_INPUT_FILENAME = '../data/pos/train.conll'
 DEV_INPUT_FILENAME = '../data/pos/dev.conll'
@@ -31,27 +31,27 @@ DEV_INPUT_FILENAME = '../data/pos/dev.conll'
 
 def main_training():
     print(f'Using TensorFlow version {tf.__version__}')
-    loader = LabeledDataLoader(TRAINING_INPUT_FILENAME)
-    tweets = loader.parse_raw_tokens_and_labels(loader.load_lines())
+    tr_loader = LabeledDataLoader(TRAINING_INPUT_FILENAME)
+    tr_tweets = tr_loader.parse_raw_tokens_and_labels(tr_loader.load_lines())
 
     tvu = TargetVocabUtil()
 
     tokenizer = BertTokenConverter(model_dir=BERT_PRETRAINED_MODEL_DIR, tvu=tvu)
-    tweets = tokenizer.convert_to_tokens(tweets)
-    tweets = tokenizer.convert_to_ids(tweets)
+    tr_tweets = tokenizer.convert_to_tokens(tr_tweets)
+    tr_tweets = tokenizer.convert_to_ids(tr_tweets)
 
     nn_input_preparer = NNInputPreparer(tvu, max_seq_len=MAX_SEQ_LEN)
-    tweets = nn_input_preparer.filter_out_long_sequences(tweets)
+    tr_tweets = nn_input_preparer.filter_out_long_sequences(tr_tweets)
 
-    print(f'Training and validating on {len(tweets)} tweets, each no longer than {MAX_SEQ_LEN} tokens')
+    print(f'Training on {len(tr_tweets)} tweets, each no longer than {MAX_SEQ_LEN} tokens')
 
-    irregular_inputs = [[item[0] for item in tweet] for tweet in tweets]
-    irregular_targets = [[item[1] for item in tweet] for tweet in tweets]
+    tr_irregular_inputs = [[item[0] for item in tweet] for tweet in tr_tweets]
+    tr_irregular_targets = [[item[1] for item in tweet] for tweet in tr_tweets]
 
-    rectangular_inputs = nn_input_preparer.rectangularize_inputs(irregular_inputs)
-    rectangular_targets = nn_input_preparer.rectangularize_targets(irregular_targets)
+    tr_rectangular_inputs = nn_input_preparer.rectangularize_inputs(tr_irregular_inputs)
+    tr_rectangular_targets = nn_input_preparer.rectangularize_targets(tr_irregular_targets)
 
-    targets_one_hot_encoded = nn_input_preparer.rectangular_targets_to_one_hot(rectangular_targets)
+    tr_targets_one_hot_encoded = nn_input_preparer.rectangular_targets_to_one_hot(tr_rectangular_targets)
 
     if CONTINUE_TRAINING:
         print('Continuing training from', TRAINING_MODEL_FILENAME_TO_CONTINUE)
@@ -69,14 +69,14 @@ def main_training():
     cp_filepath = BASE_DIR + 'ep_{epoch}_valacc_{val_accuracy:.5f}.h5'
 
     dev_loader = LabeledDataLoader(DEV_INPUT_FILENAME)
-    tweets = loader.parse_raw_tokens_and_labels(dev_loader.load_lines())
-    tweets = tokenizer.convert_to_tokens(tweets)
-    tweets = tokenizer.convert_to_ids(tweets)
-    tweets = nn_input_preparer.filter_out_long_sequences(tweets)
-    print(f'processing all {len(tweets)} not-too-long tweets from {DEV_INPUT_FILENAME}')
-    dev_irregular_inputs = [[item[0] for item in tweet] for tweet in tweets]
+    dev_tweets = dev_loader.parse_raw_tokens_and_labels(dev_loader.load_lines())
+    dev_tweets = tokenizer.convert_to_tokens(dev_tweets)
+    dev_tweets = tokenizer.convert_to_ids(dev_tweets)
+    dev_tweets = nn_input_preparer.filter_out_long_sequences(dev_tweets)
+    print(f'processing all {len(dev_tweets)} not-too-long tweets from {DEV_INPUT_FILENAME}')
+    dev_irregular_inputs = [[item[0] for item in tweet] for tweet in dev_tweets]
     # print('proportion of non-pad in dev:', sum(len(tweet) for tweet in tweets) / (MAX_SEQ_LEN * len(tweets)))
-    dev_irregular_targets = [[item[1] for item in tweet] for tweet in tweets]
+    dev_irregular_targets = [[item[1] for item in tweet] for tweet in dev_tweets]
     dev_rectangular_inputs = nn_input_preparer.rectangularize_inputs(dev_irregular_inputs)
     dev_rectangular_targets = nn_input_preparer.rectangularize_targets(dev_irregular_targets)
     dev_targets_one_hot_encoded = nn_input_preparer.rectangular_targets_to_one_hot(dev_rectangular_targets)
@@ -84,7 +84,7 @@ def main_training():
     checkpoint = ModelCheckpoint(cp_filepath, monitor='val_accuracy', verbose=1,
                                  save_best_only=False)
 
-    model.fit(rectangular_inputs, targets_one_hot_encoded, batch_size=32,
+    model.fit(tr_rectangular_inputs, tr_targets_one_hot_encoded, batch_size=32,
               initial_epoch=INITIAL_EPOCH,
               epochs=MAX_EPOCHS,
               validation_data=(dev_rectangular_inputs, dev_targets_one_hot_encoded),
